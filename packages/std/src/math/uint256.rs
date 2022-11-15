e06ba250d69b263a2c8fd8896715ddfb6ae3860e
aeryz <abdullaheryz@protonmail.com>
2022-11-15 23:47:49 +0100

    Fix no_std dependencies
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- packages/std/src/math/uint256.rs
+++ packages/std/src/math/uint256.rs
@@ -5,6 +5,7 @@ use core::ops::{
     SubAssign,
 };
 use core::str::FromStr;
+#[cfg(feature = "std")]
 use forward_ref::{forward_ref_binop, forward_ref_op_assign};
 use serde::{de, ser, Deserialize, Deserializer, Serialize};
 
@@ -459,6 +460,7 @@ impl Sub<Uint256> for Uint256 {
         )
     }
 }
+#[cfg(feature = "std")]
 forward_ref_binop!(impl Sub, sub for Uint256, Uint256);
 
 impl SubAssign<Uint256> for Uint256 {
@@ -466,6 +468,7 @@ impl SubAssign<Uint256> for Uint256 {
         *self = *self - rhs;
     }
 }
+#[cfg(feature = "std")]
 forward_ref_op_assign!(impl SubAssign, sub_assign for Uint256, Uint256);
 
 impl Div<Uint256> for Uint256 {
@@ -499,6 +502,7 @@ impl Rem for Uint256 {
         Self(self.0.rem(rhs.0))
     }
 }
+#[cfg(feature = "std")]
 forward_ref_binop!(impl Rem, rem for Uint256, Uint256);
 
 impl RemAssign<Uint256> for Uint256 {
@@ -506,6 +510,7 @@ impl RemAssign<Uint256> for Uint256 {
         *self = *self % rhs;
     }
 }
+#[cfg(feature = "std")]
 forward_ref_op_assign!(impl RemAssign, rem_assign for Uint256, Uint256);
 
 impl Mul<Uint256> for Uint256 {
@@ -519,6 +524,7 @@ impl Mul<Uint256> for Uint256 {
         )
     }
 }
+#[cfg(feature = "std")]
 forward_ref_binop!(impl Mul, mul for Uint256, Uint256);
 
 impl MulAssign<Uint256> for Uint256 {
@@ -526,6 +532,7 @@ impl MulAssign<Uint256> for Uint256 {
         *self = *self * rhs;
     }
 }
+#[cfg(feature = "std")]
 forward_ref_op_assign!(impl MulAssign, mul_assign for Uint256, Uint256);
 
 impl Shr<u32> for Uint256 {
